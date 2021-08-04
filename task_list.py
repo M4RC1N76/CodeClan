39758ad157dbc161fdeff37fcc48cd6dd6503e64
M4RC1N76 <85021019+M4RC1N76@users.noreply.github.com>
2021-08-04 19:42:08 +0100

    Add files via upload

--- task_list.py
+++ task_list.py
@@ -1,51 +1,75 @@
-# import pbd
-
 
 tasks = [
-    {"description": "Wash Dishes", "completed": False, "time_taken": 10},
-    {"description": "Clean Windows", "completed": False, "time_taken": 15},
-    {"description": "Make Dinner", "completed": True, "time_taken": 30},
-    {"description": "Feed Cat", "completed": False, "time_taken": 5},
-    {"description": "Walk Dog", "completed": True, "time_taken": 60},
+    { "description": "Wash Dishes", "completed": False, "time_taken": 10 },
+    { "description": "Clean Windows", "completed": False, "time_taken": 15 },
+    { "description": "Make Dinner", "completed": True, "time_taken": 30 },
+    { "description": "Feed Cat", "completed": False, "time_taken": 5 },
+    { "description": "Walk Dog", "completed": True, "time_taken": 60 },
 ]
-# Print a list of uncompleted tasks
-def uncompleted_tasks(task_list):
-    for task in task_list:
+
+# MVP
+
+## Get list of uncompleted tasks
+def get_uncompleted_tasks(list):
+    uncompleted_tasks = []
+    for task in list:
         if task["completed"] == False:
-            print(task)
+            uncompleted_tasks.append(task)
+    return uncompleted_tasks
 
-# uncompleted_tasks(tasks)
-
-# Print a list of completed tasks
-
-
-def completed_tasks(task_list):
-    for task in task_list:
+## Get list of completed tasks
+def get_completed_tasks(list):
+    completed_tasks = []
+    for task in list:
         if task["completed"] == True:
-            print(task)
+            completed_tasks.append(task)
+    return completed_tasks
 
 
-# completed_tasks(tasks)
 
-# Print a list of all task descriptions
-def all_tasks(task_list):
-    for task in task_list:
-        print(task["description"])
+## Refactor get_uncompleted_tasks and get_completed_tasks
+def get_tasks_by_status(list, status):
+    tasks = []
+    for task in list:
+        if task["completed"] == status:
+            tasks.append(tasks)
+    return tasks
 
-# all_tasks(tasks)
+## Get tasks where time_taken is at least a given time
+def get_tasks_taking_longer_than(list, time):
+    tasks = []
+    for task in list:
+        if task["time_taken"] >= time:
+            tasks.append(task)
+    return tasks
 
-# Print a list of tasks where time_taken is at least a (given time)? How to do it?
-#def tasks_time_taken(task_list):
- #   for task in task_list:
-  #      if task("time_taken") >= ?:
-   #         print(task["time_taken"])
+## Find any task with specific description
+def get_task_with_description(list, description):
+    for task in list:
+        if task["description"] == description:
+            return task
+    return "Task Not Found"
 
-#tasks_time_taken(tasks)
 
-# Print any task with a given description
-def given_description(task_list):
-    for task in task_list:
-        if tasks["description"] == ("description"): #???
-            print(tasks)
 
-given_description(tasks) # ???
+def mark_task_complete(task):
+    task["completed"] = True
+
+# Extensions
+
+# create a task
+def create_task(description, time_taken):
+    task = {}
+    task["description"] = description
+    task["completed"] = False
+    task["time_taken"] = time_taken
+
+    return task
+
+# add a task to the list
+def add_to_list(list, task):
+    list.append(task)
+
+# create a menu
+
+
